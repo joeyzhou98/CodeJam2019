ac3c6e779881e84bae4ca6d6e7cdec74e8067301
joeyzhou98 <joeyzhou7002@gmail.com>
2019-11-16 01:29:51 -0500

    add routes

--- run.py
+++ run.py
@@ -1,10 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for, request
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def root():
+    return redirect(url_for('home'))
+
+
+@app.route('/home')
+def home():
+    data = request.args.get("data")
+    if data is None:
+        return render_template("home.html", result="No results")
+    else:
+        return render_template("home.html", result=data)
 
 
 app.run(port=5000)
